4360801e829aec46eef9fa47a2ff75e7444a345e
Nazmul Hasan <Nazmul.Hasan@g.bracu.ac.bd>
2025-08-07 21:56:43 +0600

    .\6_single_number_2_bit_manipulation.cpp

---
+++ 6_single_number_2_bit_manipulation.cpp
@@ -0,0 +1,25 @@
+class Solution
+{
+public:
+    int singleNumber(vector<int> &nums)
+    {
+
+        int loner = 0;
+
+        for (int shift = 0; shift < 32; shift++)
+        {
+            int bitSum = 0;
+
+            for (auto &num : nums)
+            {
+
+                bitSum += (num >> shift) & 1;
+            }
+
+            int lonerBit = bitSum % 3;
+
+            loner = loner | (lonerBit << shift);
+        }
+        return loner;
+    }
+};
